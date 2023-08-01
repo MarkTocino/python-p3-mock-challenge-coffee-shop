68efa8ccdca58a666cf1093807d66661ac12d387
MarkTocino <Mark_tocino@yahoo.com>
2023-08-01 15:06:57 -0400

    Completed LABS BABY!

--- lib/classes/coffee.py
+++ lib/classes/coffee.py
@@ -1,17 +1,35 @@
 class Coffee:
     def __init__(self, name):
         self.name = name
+        self._order = []
+        self._customer = []
+
+    def get_coffee_name(self):
+        return self._name
+    def set_coffee_name(self, name):
+        if name and isinstance(name, str) and not hasattr(self, "name"):
+            self._name = name
+        else:
+            raise Exception
         
+    name = property(get_coffee_name, set_coffee_name)
     def orders(self, new_order=None):
         from classes.order import Order
-        pass
+        if new_order and isinstance(new_order, Order):
+            self._order.append(new_order)
+        return self._order
+
     
     def customers(self, new_customer=None):
         from classes.customer import Customer
-        pass
+        if new_customer and isinstance(new_customer, Customer) and new_customer not in self._customer:
+            self._customer.append(new_customer)
+        return self._customer
+
     
     def num_orders(self):
-        pass
+        return len(self._order)
     
     def average_price(self):
-        pass
+        prices = [price.price for price in self._order]
+        return sum(prices) / len(self._order)
--- lib/classes/order.py
+++ lib/classes/order.py
@@ -1,7 +1,42 @@
 
 class Order:
-
+    all = []
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
         self.price = price
+        coffee.orders(self)
+        coffee.customers(customer)
+        customer.orders(self)
+        customer.coffees(coffee)
+
+        Order.all.append(self)
+    def get_order_price(self):
+        return self._price
+    def set_order_price(self, price):
+        if price and isinstance (price, int) and 1 <= price <= 10:
+            self._price = price
+        else:
+            raise Exception
+    price = property(get_order_price, set_order_price)
+
+    def get_order_customer(self):
+        return self._customer
+    
+    def set_order_customer(self, customer):
+        from classes.customer import Customer
+        if customer and isinstance(customer, Customer):
+            self._customer = customer
+        else:
+            raise Exception
+    order_customer = property(get_order_customer, set_order_customer)
+
+    def get_order_coffee(self):
+        self._coffee
+    def set_order_coffee(self, coffee):
+        from classes.coffee import Coffee
+        if coffee and isinstance(coffee, Coffee):
+            self._coffee = coffee
+        else:
+            raise Exception
+    order_coffee= property(get_order_coffee,set_order_coffee)
